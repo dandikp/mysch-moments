0ca7297b2a16f35ae797ba7d2412dd6c5a612ac9
Dandi Karunia Pratama <dandi.karunia@gmail.com>
2019-01-28 11:32:29 +0700

    Add search button event listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,47 @@
 (function() {
 	'use strict';
-	
+
+	var lHeaderWrapper = document.getElementsByClassName('l-header--wrapper')[0];
 	var lHeader = document.getElementById('js-l-header');
 	var mobileMenuButton = document.getElementsByClassName('mobile-nav-button')[0];
 	var mobileSearchButton = document.getElementsByClassName('mobile-search-button')[0];
-});
+	var searchButton = document.getElementsByClassName('search-button')[0];
+	var searchWrapper = document.getElementsByClassName('search-form--wrapper')[0];
+	var logo = document.getElementsByClassName('main-logo--image__normal')[0];
+	var logoSticky = document.getElementsByClassName('main-logo--image__sticky')[0];
+	var stickyHeaderPos = 128;
+
+	window.scroll({
+		top: 0,
+		let: 0,
+		behavior: 'smooth'
+	});
+
+	window.addEventListener('scroll', function () {
+		let scrollPos = window.scrollY;
+
+		if (scrollPos  >= stickyHeaderPos) {
+			lHeaderWrapper.classList.add('l-header--wrapper__sticky');
+
+			logo.style.display = 'none';
+			logoSticky.style.display = 'block';
+
+
+		} else {
+			lHeaderWrapper.classList.remove('l-header--wrapper__sticky');
+
+			logo.style.display = 'block';
+			logoSticky.style.display = 'none';
+		}
+	});
+
+	searchButton.addEventListener('click', function () {
+		let isActive = searchWrapper.classList.contains('active');
+
+		if (isActive) {
+			searchWrapper.classList.remove('active');
+		} else {
+			searchWrapper.classList.add('active');
+		}
+	})
+})();
