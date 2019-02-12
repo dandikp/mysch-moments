a3c39030b634598df98aae75739a9a10ce8d423d
Dandi Karunia Pratama <dandi.karunia@gmail.com>
2019-02-12 11:23:45 +0700

    Add main slider event

--- src/js/scripts/head-slider-event.js
+++ src/js/scripts/head-slider-event.js
@@ -1,14 +1,18 @@
 (function() {
 	'use strict';
 
-	var sliderWrapper = document.querySelector('.js-flickity-head-slider');
+	var slideBackgroundWrapper = document.getElementsByClassName('head-slide_background_wrapper');
 
-	var flickity = new Flickity(sliderWrapper, {
-		cellAlign: 'left',
-		// arrowShape: "M10.273,5.009c0.444-0.444,1.143-0.444,1.587,0c0.429,0.429,0.429,1.143,0,1.571l-8.047,8.047h26.554"
-		// 	+ "c0.619,0,1.127,0.492,1.127,1.111c0,0.619-0.508,1.127-1.127,1.127H3.813l8.047,8.032c0.429,0.444,0.429,1.159,0,1.587"
-		// 	+ "c-0.444,0.444-1.143,0.444-1.587,0l-9.952-9.952c-0.429-0.429-0.429-1.143,0-1.571L10.273,5.009z",
-		contain: true
+	window.addEventListener('scroll', function () {
+		Array.prototype.forEach.call(slideBackgroundWrapper, function(el, index, array) {
+			let scrollPos = window.scrollY;
+			let slideVerticalTransform = scrollPos * 0.26;
+			let screenHeight = window.innerHeight;
+
+			if (scrollPos <= screenHeight) {
+				el.style.transform = 'translate3d(0px, ' + slideVerticalTransform + 'px, 0px)';
+			}
+		});
 	});
 })();
 
