85a553d80a9ad08ab5d7e888ee297f907a0dff67
Dandi Karunia Pratama <dandi.karunia@gmail.com>
2019-02-19 08:52:25 +0700

    Add head slider siema config

--- src/js/siema.config.js
+++ src/js/siema.config.js
@@ -1,34 +1,15 @@
 (function() {
-	'use strict';	
-
-	var headSlideItems = document.getElementsByClassName('js-head-slide');
-	
+	'use strict';
+		
 	// Head Slider
 	var siemaHeadSlider = new Siema({
 		selector: '.js-head-slider',
 		loop: true,
 		duration: 0,
-		easing: 'cubic-bezier(0.895, 0.03, 0.685, 0.22)'
+		perPage: 1,
+		startIndex: 0,
 	});
 
 	siemaHeadSlider.detachEvents();
-	siemaHeadSlider.attachResizeEvent();
-
-	Array.prototype.forEach.call(headSlideItems, function(el, index, array){
-		el.addEventListener('touchstart', function (event) {
-			// siemaHeadSlider.next();
-		});
-
-		el.addEventListener('touchmove', function (event) {
-			// siemaHeadSlider.next();
-		});
-		
-		el.addEventListener('touchend', function (event) {
-			// siemaHeadSlider.next();
-		});
-	});
-
-	// setInterval(function() {
-	// 	
-	// }, 3000);
+	siemaHeadSlider.startHeadSlider();
 })();
